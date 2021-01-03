b1ba7949132abc0cb28fc61f021b4c6ff5be8bdc
Uriel Yair <uriel91@gmail.com>
2021-01-03 14:14:25 +0200

    set CurrencyExchange as starting point for rendering.

---
+++ src/components/CurrencyExchangeApp.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class CurrencyExchangeApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+    };
+  }
+
+  
+
+  render() {
+    return (
+      <div className='App'>
+        <header>
+          <h1>Currency Exchange App</h1>
+				</header>
+      </div>
+    );
+  }
+}
+export default CurrencyExchangeApp;
