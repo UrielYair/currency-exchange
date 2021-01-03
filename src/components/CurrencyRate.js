ace922e053d5876e07235ee94bd260209c9f56d6
Uriel Yair <uriel91@gmail.com>
2021-01-03 18:00:56 +0200

    define event handlers for buttons.

--- src/components/CurrencyRate.js
+++ src/components/CurrencyRate.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
-const CurrencyRate = ({name, rate, id}) => {
+const CurrencyRate = ({currency, name, rate, currencies, setCurrencies}) => {
+    
+    const deleteHandler = () => {
+        // console.log(currency);
+        setCurrencies(currencies.filter( el => el.id !== currency.id))
+    }
+
+    const checkHandler = () => {
+        setCurrencies(currencies.map((item) => {
+            if(item.id === currency.id) {
+                return {
+                    ...item, checked: !item.checked
+                }
+            }
+            return item;
+        }))
+    }
+
     return (
         <div className='currency'>
-            <li className= 'currency-item'>{name} {rate}</li>
-            <button className='trash-btn'>
+            <li className= {`currency-item ${currency.checked ? "checked" : "" }`}>{name} {rate}</li>
+            <button onClick={checkHandler} className='complete-btn'>
+                <i className='fas fa-check'></i>
+            </button>
+            <button onClick={deleteHandler} className='trash-btn'>
                 <i className='fas fa-trash'></i>
             </button>
         </div>
