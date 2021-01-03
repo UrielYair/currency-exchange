2d6d831f18cd3df46ad86176d7e7a7f903d65f40
Uriel Yair <uriel91@gmail.com>
2021-01-03 16:28:19 +0200

    Add CurrencyRate (currency-item).

---
+++ src/components/CurrencyRate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CurrencyRate = () => {
+    return (
+        <div className='currency'>
+            <li className= 'currency-item'>Uriel</li>
+            <button className='trash-btn'>
+                <i className='fas fa-trash'></i>
+            </button>
+        </div>
+    );
+}
+export default CurrencyRate;
