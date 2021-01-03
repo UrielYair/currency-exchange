3a99ed68ca65299b4b02e405d7d48117389b103b
Uriel Yair <uriel91@gmail.com>
2021-01-03 15:50:28 +0200

    add submit functionality to form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,36 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
-const Form = ({ setInputText }) => {
-  const inputTextHandler = (e) => {
-    setInputText(e.target.value);
+const Form = ({ 
+  currencies, setCurrencies,
+  currencyName, setCurrencyName,
+  currencyRate, setCurrencyRate
+ }) => {
+  const currencyNameHandler = (e) => {
+    setCurrencyName(e.target.value);
+  }
+  const currencyRateHandler = (e) => {
+    setCurrencyRate(e.target.value);
+  }
+  const submitCurrencyHandler = (e) => {
+    e.preventDefault();
+    setCurrencies(
+      [...currencies, {
+        currency_name: currencyName, 
+        currency_rate: currencyRate, 
+        deleted: false, 
+        id: uuidv4()
+      }])
+      setCurrencyName('');
+      setCurrencyRate('');
   }
 
   return (
     <form>
-      <input onChange= {inputTextHandler} type="text" className="currency-input" />
-      <button className="currency-button" type="submit">
+      <input value={currencyName} onChange= {currencyNameHandler} type="text" className="currency-input" placeholder='currency name'/>
+      <input value={currencyRate} onChange= {currencyRateHandler} type="text" className="currency-input" placeholder='currency rate'/>
+    
+      <button className="currency-button" type="submit" onClick={submitCurrencyHandler}>
         <i className="fas fa-plus-square"></i>
       </button>
     </form>
